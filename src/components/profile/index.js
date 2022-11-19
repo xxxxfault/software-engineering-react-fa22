2324e292560171486a19c1d114832c96bbe82440
xxxxfault <jiaqian0730@gmail.com>
2022-11-19 13:46:20 -0500

    likes

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,13 +1,38 @@
-import React from "react";
-import Tuits from "../tuits";
-import {Link} from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import MyTuits from "../profile/my-tuits";
+import { Link, Route, Routes, useNavigate, useLocation} from "react-router-dom";
+import * as service from "../../services/auth-service";
+import TuitsAndReplies
+  from "./tuits-and-replies";
+import Media from "./media";
+import MyLikes from "./my-likes";
 
 const Profile = () => {
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [profile, setProfile] = useState({});
+  useEffect(async () => {
+    try {
+      const user = await service.profile();
+      setProfile(user);
+    } catch (e) {
+      navigate('/login');
+    }
+  }, []);
+  const logout = () => {
+    service.logout()
+        .then(() => navigate('/login'));
+  }
+
   return(
+
     <div className="ttr-profile">
       <div className="border border-bottom-0">
-        <h4 className="p-2 mb-0 pb-0 fw-bolder">NASA<i className="fa fa-badge-check text-primary"></i></h4>
-        <span className="ps-2">67.6K Tuits</span>
+
+
+
+
         <div className="mb-5 position-relative">
           <img className="w-100" src="../images/nasa-profile-header.jpg"/>
           <div className="bottom-0 left-0 position-absolute">
@@ -21,20 +46,24 @@ const Profile = () => {
             Edit profile
           </Link>
         </div>
+        <button onClick={logout} className="mt-2 float-end btn btn-warning rounded-pill">
+          Logout
+        </button>
 
         <div className="p-2">
           <h4 className="fw-bolder pb-0 mb-0">
-            NASA<i className="fa fa-badge-check text-primary"></i>
+            {profile.username}<i className="fa fa-badge-check text-primary"></i>
           </h4>
-          <h6 className="pt-0">@NASA</h6>
+          <h6 className="pt-0">@{profile.username}</h6>
           <p className="pt-2">
             There's space for everybody. Sparkles
           </p>
+          <span className="ps-2">67.6K Tuits</span>
           <p>
             <i className="far fa-location-dot me-2"></i>
             Pale Blue Dot
             <i className="far fa-link ms-3 me-2"></i>
-            <a href="nasa.gov" className="text-decoration-none">nasa.gov</a>
+            <a href="https://www.nasa.gov/" className="text-decoration-none">nasa.gov</a>
             <i className="far fa-balloon ms-3 me-2"></i>
             Born October 1, 1958
             <br/>
@@ -45,8 +74,8 @@ const Profile = () => {
           <b className="ms-4">51.1M</b> Followers
           <ul className="mt-4 nav nav-pills nav-fill">
             <li className="nav-item">
-              <Link to="/profile/tuits"
-                    className="nav-link active">
+              <Link to="/profile/mytuits"
+                    className={`nav-link ${location.pathname.indexOf('mytuits') >= 0 ? 'active':''}`}>
                 Tuits</Link>
             </li>
             <li className="nav-item">
@@ -60,14 +89,25 @@ const Profile = () => {
                 Media</Link>
             </li>
             <li className="nav-item">
-              <Link to="/profile/likes"
+              <Link to="/profile/mylikes"
                     className="nav-link">
                 Likes</Link>
             </li>
           </ul>
         </div>
       </div>
-      <Tuits/>
+      <Routes>
+        <Route path="/mytuits"
+               element={<MyTuits/>}/>
+        <Route path="/tuits-and-replies"
+               element={<TuitsAndReplies/>}/>
+        <Route path="/media"
+               element={<Media/>}/>
+        <Route path="/mylikes"
+               element={<MyLikes/>}/>
+        {/*<Route path="/*"*/}
+        {/*       element={<Profile/>}/>*/}
+      </Routes>
     </div>
   );
 }
