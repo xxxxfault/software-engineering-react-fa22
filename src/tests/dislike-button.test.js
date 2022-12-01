f74b5109bd4c1ba3b0d81d81626969fa6fe4c863
xxxxfault <jiaqian0730@gmail.com>
2022-12-01 13:28:37 -0500

    add test

--- src/tests/dislike-button.test.js
+++ src/tests/dislike-button.test.js
@@ -1,14 +1,14 @@
 import {createUser, deleteUsersByUsername} from "../services/users-service";
-import {createTuit, createTuitByUser, deleteTuit} from "../services/tuits-service";
+import { createTuitByUser, deleteTuit, findTuitById} from "../services/tuits-service";
 import {userDislikesTuit, userTogglesTuitDislikes} from "../services/dislike-service";
-import {login, signup} from "../services/auth-service";
+
 
 // mocked user and tuit to test dislike button
 const mockTuit={tuit:'tuit for dislike button'};
 const mockUser={username:'user',password:'passpass',credential: 'me',email:'123@gmail.com'}
 
 
-describe('test  dislike button works',()=>{
+describe('test dislike button works',()=>{
 
     beforeAll(()=>{
         return deleteUsersByUsername(mockUser.username);
@@ -18,7 +18,7 @@ describe('test  dislike button works',()=>{
         return deleteUsersByUsername(mockUser.username);
     })
 
-    test('test  dislike button works',async ()=>{
+    test('test dislike button works',async ()=>{
         // create new user
         const userInserted = await createUser(mockUser);
 
@@ -27,12 +27,14 @@ describe('test  dislike button works',()=>{
 
         // dislike that tuit
         await userDislikesTuit(userInserted._id, tuitInserted._id);
-        expect(tuitInserted.stats.dislikes).toEqual(1);
+        const tuitDislikes= await findTuitById(tuitInserted._id)
+        expect(tuitDislikes.stats.dislikes).toEqual(1);
 
 
         // undislike that tuit
         await userTogglesTuitDislikes(userInserted._id, tuitInserted._id);
-        expect(tuitInserted.stats.dislikes).toEqual(0);
+        const tuitUndislikes= await findTuitById(tuitInserted._id)
+        expect(tuitUndislikes.stats.dislikes).toEqual(0)
 
         // clean up tuit inserted
         await deleteTuit(tuitInserted._id);
